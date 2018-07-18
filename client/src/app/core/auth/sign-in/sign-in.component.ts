15c1b3aafaf26cd1e8b6bf3eee7def7305173705
Somebuddy <oleksiy.drogovozov@gmail.com>
2018-07-18 22:18:58 +0300

    Add sign in form initial template

--- client/src/app/core/auth/sign-in/sign-in.component.ts
+++ client/src/app/core/auth/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'tb-sign-in',
@@ -7,10 +8,22 @@ import { AuthService } from '../shared/auth.service';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  submitError = null;
 
   constructor(public auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  signin(form: NgForm) {
+    const { email, password } = form.value;
+    this.auth.signUpWithEmail(email, password)
+      .then(() => {
+        console.log('User signed in');
+      })
+      .catch(error => {
+        console.log('User sign in error', error);
+      });
   }
 
 }
